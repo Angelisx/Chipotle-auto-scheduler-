a65f670a2731c72d42b9b66c6da66318dd84080d
IBMAngel <43751883+Angelisx@users.noreply.github.com>
2023-11-20 13:14:23 -0500

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -3,7 +3,6 @@ from simplegmail.query import construct_query
 from datetime import datetime
 import os.path
 import re
-import base64
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
