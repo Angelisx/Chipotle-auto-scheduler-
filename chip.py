7a9039d2806a2cb08175aee185f11f9195b64e26
Angelisx <bladetax132@gmail.com>
2024-02-01 03:50:02 -0500

    added an in progress user interface function
    as well as added a calendar id listing function to grab the available calendars from user.
    
    i have to work on moving the calendar id data to the UI allowing the user to select from a dropdown box

--- chip.py
+++ chip.py
@@ -9,62 +9,137 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from datetime import datetime
+from customtkinter import *
 
 gmail = Gmail()
 processed_dates = set()
-# calendarId='1d3014ce0c421b0ba9eb3ae682e681daaf522a492bc89ce09c7b54c8a583880d@group.calendar.google.com'
 
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 
-def get_CHIP_messages():
-    # For even more control, use queries:
-    query_params = {
-        "labels": [["CHIP"]]
-    }
 
-    messages = gmail.get_messages(query=construct_query(query_params))
+def user_interface(calendar_list_summary):
+    app = CTk()
+    app.geometry("400x400")
 
-    for message in messages:
-        process_email(message)
-        # print(message.plahin)
+    dropbox = CTkComboBox(master=app, values=[calendar_list_summary, calendar_list_summary, calendar_list_summary], fg_color="black", bg_color="white", dropdown_fg_color="red")
 
-def process_email(email):
-    body = email.html  
+    dropbox.place(x=100, y=100, anchor="center")
+
+    app.mainloop()
 
 
-    # Extract schedule data using regular expressions
-    pattern =re.compile(r'\s*(\d{1,2}/\d{1,2}/\d{4}) Shift: (\d{1,2}:\d{2} [APMapm]{2}) - (\d{1,2}:\d{2} [APMapm]{2})')
+
+# def get_CHIP_messages():
+#     # For even more control, use queries:
+#     query_params = {
+#         "labels": [["CHIP"]]
+#     }
+
+#     messages = gmail.get_messages(query=construct_query(query_params))
+
+#     for message in messages:
+#         process_email(message)
+#         # print(message.plahin)
+
+# def process_email(email):
+#     body = email.html  
+
+
+#     # Extract schedule data using regular expressions
+#     pattern =re.compile(r'\s*(\d{1,2}/\d{1,2}/\d{4}) Shift: (\d{1,2}:\d{2} [APMapm]{2}) - (\d{1,2}:\d{2} [APMapm]{2})')
    
 
-    schedule_data = pattern.findall(body)
-    for date, start_time, end_time in schedule_data:
-        # print(f'Date: {date}, Start Time: {start_time}, End Time: {end_time}')
-        if date not in processed_dates:
-            processed_dates.add(date)
-            # print(f'Date: {date}, Start Time: {start_time}, End Time: {end_time}')
+#     schedule_data = pattern.findall(body)
+#     for date, start_time, end_time in schedule_data:
+#         # print(f'Date: {date}, Start Time: {start_time}, End Time: {end_time}')
+#         if date not in processed_dates:
+#             processed_dates.add(date)
+#             # print(f'Date: {date}, Start Time: {start_time}, End Time: {end_time}')
 
             
-            d = datetime.strptime(date, "%m/%d/%Y")
-            formatted_date = d.strftime("%Y-%m-%d")
+#             d = datetime.strptime(date, "%m/%d/%Y")
+#             formatted_date = d.strftime("%Y-%m-%d")
 
-            nt = datetime.strptime(start_time, "%I:%M %p")
-            formatted_start_time = nt.strftime("%H:%M")
+#             nt = datetime.strptime(start_time, "%I:%M %p")
+#             formatted_start_time = nt.strftime("%H:%M")
 
-            et = datetime.strptime(end_time, "%I:%M %p")
-            formatted_end_time = et.strftime("%H:%M")
+#             et = datetime.strptime(end_time, "%I:%M %p")
+#             formatted_end_time = et.strftime("%H:%M")
             
             
-            add_event_to_calendar(formatted_date,formatted_start_time, formatted_end_time)
+#             add_event_to_calendar(formatted_date,formatted_start_time, formatted_end_time)
+
+# def add_event_to_calendar(formatted_date,formatted_start_time,formatted_end_time):
+#     creds = None
+#     if os.path.exists("token.json"):
+#         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+
+#     if not creds or not creds.valid:
+#         if creds and creds.expired and creds.refresh_token:
+#             creds.refresh(Request())
+#         else:
+#             flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+#             creds = flow.run_local_server(port=0)
+
+#         with open("token.json", "w") as token:
+#             token.write(creds.to_json())
+
+#     try:
+#         service = build("calendar", "v3", credentials=creds)
+
+#         summary = 'Chipotle Schedule'
+#         location = '6135 Junction Blvd., Queens, NY 11374'
+
+#         # Format date, start_time, and end_time into a valid format for Google Calendar
+#         start_datetime = f'{formatted_date}T{formatted_start_time}:00-05:00'
+#         end_datetime = f'{formatted_date}T{formatted_end_time}:00-05:00'
 
 
-        
+#         # Check if the event already exists in the calendar
+#         events_result = service.events().list(
+#             calendarId='f6ab48a7569f8fd42cc20a93dee4b689fd2182c091c7f1476e0ad2c8f00684a9@group.calendar.google.com',
+#             timeMin=start_datetime,
+#             timeMax=end_datetime,
+#             singleEvents=True,
+#             orderBy='startTime'
+#         ).execute()
 
-def add_event_to_calendar(formatted_date,formatted_start_time,formatted_end_time):
+#         events = events_result.get('items', [])
+
+#         if not events:
+#             # Event does not exist, so add it to the calendar
+#             event = {
+#                 'summary': summary,
+#                 'location': location,
+#                 'description': 'Chipotle Schedule',
+#                 'start': {
+#                     'dateTime': start_datetime,
+#                     'timeZone': 'America/New_York',
+#                 },
+#                 'end': {
+#                     'dateTime': end_datetime,
+#                     'timeZone': 'America/New_York',
+#                 },
+#             }
+
+#             event = service.events().insert(calendarId='f6ab48a7569f8fd42cc20a93dee4b689fd2182c091c7f1476e0ad2c8f00684a9@group.calendar.google.com', body=event).execute()
+            
+#             print(f"DATE:{formatted_date}  START TIME:{start_datetime} END TIME:{end_datetime} ")
+
+#             print('Event added successfully')
+#         else:
+#             print('Event already exists')
+
+#     except HttpError as error:
+#         print(f"An error occurred: {error}")
+
+# Call the function to process CHIP messages
+
+def List_caledar_ids():
     creds = None
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -74,56 +149,29 @@ def add_event_to_calendar(formatted_date,formatted_start_time,formatted_end_time
 
         with open("token.json", "w") as token:
             token.write(creds.to_json())
-
+    
     try:
         service = build("calendar", "v3", credentials=creds)
+        
+        calendar_list = service.calendarList().list().execute()
+        for calendar_list_entry in calendar_list['items']:
+            print(f"Calendar: {calendar_list_entry['summary']}, ID: {calendar_list_entry['id']}")
 
-        summary = 'Chipotle Schedule'
-        location = '6135 Junction Blvd., Queens, NY 11374'
-
-        # Format date, start_time, and end_time into a valid format for Google Calendar
-        start_datetime = f'{formatted_date}T{formatted_start_time}:00-05:00'
-        end_datetime = f'{formatted_date}T{formatted_end_time}:00-05:00'
-
-
-        # Check if the event already exists in the calendar
-        events_result = service.events().list(
-            calendarId='f6ab48a7569f8fd42cc20a93dee4b689fd2182c091c7f1476e0ad2c8f00684a9@group.calendar.google.com',
-            timeMin=start_datetime,
-            timeMax=end_datetime,
-            singleEvents=True,
-            orderBy='startTime'
-        ).execute()
-
-        events = events_result.get('items', [])
-
-        if not events:
-            # Event does not exist, so add it to the calendar
-            event = {
-                'summary': summary,
-                'location': location,
-                'description': 'Chipotle Schedule',
-                'start': {
-                    'dateTime': start_datetime,
-                    'timeZone': 'America/New_York',
-                },
-                'end': {
-                    'dateTime': end_datetime,
-                    'timeZone': 'America/New_York',
-                },
-            }
-
-            event = service.events().insert(calendarId='f6ab48a7569f8fd42cc20a93dee4b689fd2182c091c7f1476e0ad2c8f00684a9@group.calendar.google.com', body=event).execute()
-            
-            print(f"DATE:{formatted_date}  START TIME:{start_datetime} END TIME:{end_datetime} ")
-
-            print('Event added successfully')
-        else:
-            print('Event already exists')
-
+            calendar_list_summary = calendar_list_entry['summary']
+            user_interface(calendar_list_summary)
     except HttpError as error:
+
         print(f"An error occurred: {error}")
 
-# Call the function to process CHIP messages
-get_CHIP_messages()
+    user_interface(calendar_list_summary)
+
+        
+
+
+List_caledar_ids()
+
+# get_CHIP_messages()
+
+
+
 
